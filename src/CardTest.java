5891da6460939e71ea5c60d334551f6e37416517
Bongani Sibanda <sibandabongz@gmail.com>
2020-01-22 20:44:00 +0200

    update Card tests

--- src/CardTest.java
+++ src/CardTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 
 public class CardTest {
@@ -6,5 +7,13 @@ public class CardTest {
     @Test(expected = IllegalArgumentException.class)
     public void testIllegalCards() {
         new Card(14, Card.HEARTS);
+    }
+
+    @Test
+    public void testGetSuit() {
+        assertEquals(Card.DIAMONDS, new Card(14, Card.DIAMONDS).getSuit());
+        assertEquals(Card.SPADES, new Card(14, Card.SPADES).getSuit());
+        assertEquals(Card.CLUBS, new Card(14, Card.CLUBS).getSuit());
+        assertEquals(Card.HEARTS, new Card(14, Card.HEARTS).getSuit());
     }
 }
